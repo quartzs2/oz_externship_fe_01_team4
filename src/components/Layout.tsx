43b014b6f41fa044445f738fffdbf72cb2c54282
quartzs2 <csh001231@gmail.com>
2025-06-19 19:06:38 +0900

    feat: react-router 기본 설정(#9)
    - react-router 라이브러리 추가
    - App.tsx에 라우팅 적용
    - main.tsx에 BrowserRouter 적용
    - Layout.tsx 추가
    - Main.tsx 추가
    - NotFound.tsx 추가

---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router'
+
+const Layout = () => {
+  return (
+    <>
+      {/* 컴포넌트 넣어주세요 */}
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
+export default Layout
