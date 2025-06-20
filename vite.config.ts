0d3bdbe1e6cbc4da4f6d063940dd95613a647b13
idubusomuch <idubusomuch@gmail.com>
2025-06-20 22:28:19 +0900

    feat: 공통 Icon 컴포넌트 및 SVG 설정 추가 (#20) - Icon 컴포넌트 생성 및 타입 분리 - Tailwind 기반 size/color 처리 방식 구현 - SVG 파일 import를 위한 vite 설정 및 타입 선언 (vite-env.d.ts) 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,5 +19,5 @@ export default defineConfig({
       '@store': resolve(__dirname, 'src/store'),
     },
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
 })
