5d71dfdc9401d8a779606f142f79dcc61c50837b
LHM52 <dlgudals8410@naver.com>
2025-06-23 02:03:37 +0900

    feat: 표(테이블) 공통 컴포넌트 구현(#16)
    - 공통 컴포넌트 기본기능 구현완료
    
    - 기본 기능 : 표제목 입력, 받아오는 데이터에 따라 렌더링, 기본 UI 구현
    
    - 표 데이터 렌더링 테스트 완료
    
    - 페이지네이션 구현 완료
    
    - 체크박스 구현 완료
    
    - 정렬기능 구현완료
    
    - id 타입 string 제거
    
    - 데이터테이블,페이지네이션 컴포넌트 분리
    
    - 정렬기능 수정
    
    - 불필요 코드 제거
    
    - 배포, 배포 토글 스위치 UI 구현

---
+++ src/components/dataTable/TableHeader.tsx
@@ -0,0 +1,56 @@
+import type { TableHeader } from '@customType/table'
+
+type Props = {
+  headers: TableHeader[]
+  isCheckBox?: boolean
+  isAllChecked: boolean
+  sortKeys: string[]
+  sortKey: string | null
+  sortOrder: 'asc' | 'desc'
+  onSort: (key: string) => void
+  onToggleAll: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function TableHeader({
+  headers,
+  isCheckBox,
+  sortKeys,
+  sortKey,
+  sortOrder,
+  onSort,
+  isAllChecked,
+  onToggleAll,
+}: Props) {
+  return (
+    <thead className="h-[50px] bg-gray-100">
+      <tr>
+        {isCheckBox && (
+          <th className="w-12 border-b border-gray-300 p-2 text-center">
+            <input
+              type="checkbox"
+              checked={isAllChecked}
+              onChange={onToggleAll}
+            />
+          </th>
+        )}
+        {headers.map((header) => (
+          <th
+            key={header.dataKey}
+            className="border-b border-gray-300 p-2 text-center"
+          >
+            <div className="flex items-center justify-center gap-1">
+              <span>{header.text}</span>
+              {sortKeys.includes(header.dataKey) && (
+                <button onClick={() => onSort(header.dataKey)}>
+                  {sortKey === header.dataKey && sortOrder === 'desc'
+                    ? '▼'
+                    : '▲'}
+                </button>
+              )}
+            </div>
+          </th>
+        ))}
+      </tr>
+    </thead>
+  )
+}
