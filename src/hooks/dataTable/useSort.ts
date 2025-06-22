f06c8c7affc3d86fd4f860ee5821c8d620de4c4e
LHM52 <dlgudals8410@naver.com>
2025-06-23 02:03:37 +0900

    feat: 표(테이블) 공통 컴포넌트 구현(#16)
    - 공통 컴포넌트 기본기능 구현완료
    
    - 기본 기능 : 표제목 입력, 받아오는 데이터에 따라 렌더링, 기본 UI 구현
    
    - 표 데이터 렌더링 테스트 완료
    
    - 페이지네이션 구현 완료
    
    - 체크박스 구현 완료
    
    - 정렬기능 구현완료
    
    - id 타입 string 제거
    
    - 데이터테이블,페이지네이션 컴포넌트 분리
    
    - 정렬기능 수정
    
    - 불필요 코드 제거
    
    - 배포, 배포 토글 스위치 UI 구현

--- src/hooks/dataTable/useSort.ts
+++ src/hooks/dataTable/useSort.ts
@@ -1,6 +1,7 @@
 import { useState, useMemo } from 'react'
 import type { TableRowData } from '@customType/table'
 
+<<<<<<< HEAD
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
@@ -11,6 +12,11 @@ export type SortOrder = (typeof SORT_ORDER)[keyof typeof SORT_ORDER]
 export function useSort(initialData: TableRowData[]) {
   const [sortKey, setSortKey] = useState<string | null>(null)
   const [sortOrder, setSortOrder] = useState<SortOrder>(SORT_ORDER.ASC)
+=======
+export function useSort(initialData: TableRowData[]) {
+  const [sortKey, setSortKey] = useState<string | null>(null)
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc')
+>>>>>>> 663903e (feat: 표(테이블) 공통 컴포넌트 구현(#16))
 
   const sortedData = useMemo(() => {
     if (!sortKey) {
@@ -22,9 +28,15 @@ export function useSort(initialData: TableRowData[]) {
       const valB = b[sortKey]
 
       if (typeof valA === 'number' && typeof valB === 'number') {
+<<<<<<< HEAD
         return sortOrder === SORT_ORDER.ASC ? valA - valB : valB - valA
       }
       return sortOrder === SORT_ORDER.ASC
+=======
+        return sortOrder === 'asc' ? valA - valB : valB - valA
+      }
+      return sortOrder === 'asc'
+>>>>>>> 663903e (feat: 표(테이블) 공통 컴포넌트 구현(#16))
         ? String(valA).localeCompare(String(valB), undefined, { numeric: true })
         : String(valB).localeCompare(String(valA), undefined, { numeric: true })
     })
@@ -32,12 +44,21 @@ export function useSort(initialData: TableRowData[]) {
   }, [initialData, sortKey, sortOrder])
 
   const sortByKey = (key: string) => {
+<<<<<<< HEAD
     let newOrder: SortOrder
 
     if (sortKey === key) {
       newOrder = sortOrder === SORT_ORDER.ASC ? SORT_ORDER.DESC : SORT_ORDER.ASC
     } else {
       newOrder = SORT_ORDER.DESC
+=======
+    let newOrder: 'asc' | 'desc'
+
+    if (sortKey === key) {
+      newOrder = sortOrder === 'asc' ? 'desc' : 'asc'
+    } else {
+      newOrder = 'desc'
+>>>>>>> 663903e (feat: 표(테이블) 공통 컴포넌트 구현(#16))
     }
 
     setSortKey(key)
