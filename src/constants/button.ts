eb47a46b7294453cd45ecad9e23005001132d083
jihwan0492421 <tommy040201@gmail.com>
2025-06-25 17:51:06 +0900

    feat: 버튼 구현(#17)

--- src/constants/button.ts
+++ src/constants/button.ts
@@ -1,8 +1,8 @@
 export const BUTTON_VARIANTS = {
-    VARIANT1: 'bg-[#522193] text-white ',
-    VARIANT2: 'bg-[#86838B] text-white ',
-    VARIANT3: 'bg-[#FFFFFF] text-black border border-gray-300 rounded-[3px]',
-    VARIANT4: 'bg-[#CC0A0A] text-white ',
+    VARIANT1:'bg-[#522193] text-white ',
+    VARIANT2:'bg-[#86838B] text-white ',
+    VARIANT3:'bg-[#FFFFFF] text-black border border-gray-300 rounded-[3px]',
+    VARIANT4:'bg-[#CC0A0A] text-white ',
 } as const;
 
 export const DEFAULT_BUTTON_VARIANT = 'VARIANT1';
