1dac680b2f13a284ec2564f2d093bd36e0f32ecb
jihwan0492421 <tommy040201@gmail.com>
2025-06-25 14:54:40 +0900

    feat: 버튼 구현(#17)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,33 +1,31 @@
-import { BUTTON_VARIANTS, DEFAULT_BUTTON_VARIANT } from "@constants/button";
-import { cn } from "@utils/cn"
-
+import { BUTTON_VARIANTS, DEFAULT_BUTTON_VARIANT } from '@constants/button'
+import { cn } from '@utils/cn'
 
 type ButtonVariant = keyof typeof BUTTON_VARIANTS
 
-
 type ButtonProps = {
-  children: React.ReactNode;
-  onClick?: () => void;
-  disabled?: boolean;
-  variant?: ButtonVariant; 
-  className?: string;
-};
+  children: React.ReactNode
+  onClick?: () => void
+  disabled?: boolean
+  variant?: ButtonVariant
+  className?: string
+}
 
 const Button: React.FC<ButtonProps> = ({
   children,
   onClick,
   disabled = false,
-  variant = DEFAULT_BUTTON_VARIANT, 
-  className
-}) => { 
+  variant = DEFAULT_BUTTON_VARIANT,
+  className,
+}) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       className={cn(
-        'px-4 py-2 font-semibold w-[55px] h-[36px]',
+        'h-[36px] w-[55px] px-4 py-2 font-semibold',
         BUTTON_VARIANTS[variant],
-        {' cursor-not-allowed' : disabled},
+        { 'cursor-not-allowed': disabled },
         className
       )}
     >
--- src/constants/button.ts
+++ src/constants/button.ts
@@ -1,4 +1,5 @@
 export const BUTTON_VARIANTS = {
+<<<<<<< HEAD
     VARIANT1:'bg-[#522193] text-white',
     VARIANT2:'bg-[#86838B] text-white',
     VARIANT3:'bg-[#FFFFFF] text-black border border-gray-300 rounded-[3px]',
@@ -6,3 +7,12 @@ export const BUTTON_VARIANTS = {
 } as const;
 
 export const DEFAULT_BUTTON_VARIANT = 'VARIANT1';
+=======
+    REGISTER: 'bg-purple-700 hover:bg-purple-800 text-white top-5 left-5',
+    GRAY_DELETE: 'bg-gray-500 hover:bg-gray-600 text-white top-[76px] left-5',
+    CANCEL: 'bg-white text-black border border-gray-300 top-[132px] left-5 rounded-[3px]',
+    DELETE: 'bg-red-500 hover:bg-red-600 text-white top-[188px] left-5',
+} as const;
+
+export const DEFAULT_BUTTON_VARIANT = 'REGISTER';
+>>>>>>> d81f284 (feat: 버튼 구현(#17))
