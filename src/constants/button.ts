684f84ff180d43dadec42808e58b87c7fd07a16a
tkdgheee <sangho135790@gmail.com>
2025-06-26 01:17:37 +0900

    feat: 토스트 컴포넌트 구현 (#6)
    - 병합 충돌 사항 수정

--- src/constants/button.ts
+++ src/constants/button.ts
@@ -1,13 +1,4 @@
 export const BUTTON_VARIANTS = {
-<<<<<<< HEAD
-    VARIANT1:'bg-[#522193] text-white',
-    VARIANT2:'bg-[#86838B] text-white',
-    VARIANT3:'bg-[#FFFFFF] text-black border border-gray-300 rounded-[3px]',
-    VARIANT4:'bg-[#CC0A0A] text-white',
-} as const;
-
-export const DEFAULT_BUTTON_VARIANT = 'VARIANT1';
-=======
   VARIANT1: 'bg-[#522193] text-white',
   VARIANT2: 'bg-[#86838B] text-white',
   VARIANT3: 'bg-[#FFFFFF] text-black border border-gray-300 rounded-[3px]',
@@ -15,4 +6,3 @@ export const DEFAULT_BUTTON_VARIANT = 'VARIANT1';
 } as const
 
 export const DEFAULT_BUTTON_VARIANT = 'VARIANT1'
->>>>>>> 79b3fdf (feat: 토스트 컴포넌트 구현 (#6))
