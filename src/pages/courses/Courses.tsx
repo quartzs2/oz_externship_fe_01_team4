b7aa051c1484e2a45dbdfb2cefa85d1954dac348
dltmfql456 <dltmfql456@naver.com>
2025-07-09 13:55:59 +0900

    feat: 과정목록 조회 기능(#104)
    - api 연동
    - urls.ts 세부 경로 courses 추가

--- src/pages/courses/Courses.tsx
+++ src/pages/courses/Courses.tsx
@@ -1,46 +1,74 @@
 import Button from '@components/common/Button'
 import DataTable from '@components/common/data-table/DataTable'
 import Pagination from '@components/common/data-table/Pagination'
+import { ADMIN_API_BASE_URL, ADMIN_API_PATH } from '@constants/urls'
+import type { TableRowData } from '@custom-types/table'
 import { usePagination } from '@hooks/data-table/usePagination'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const ACCESS_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUyMTIyMTg4LCJpYXQiOjE3NTIwMzU3ODgsImp0aSI6IjUwODQ5Nzc2NmIxNzRkMTk4Yzc4YjNmN2JhMDRlOGUzIiwidXNlcl9pZCI6MTJ9.-vbrx_gHsU9XaIrtdFjjmB3Ema-IoI9dCJA2Tp98Mh0'
 
 // 페이지 상수 추가
 const COUNT_LIMIT = 20
 
+// 공통 API 인스턴스
+const apiClient = axios.create({
+  baseURL: ADMIN_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+  },
+})
+
+const fetchAPI = async () => {
+  const res = await apiClient.get(ADMIN_API_PATH.COURSES)
+  console.log(res)
+  return res.data.results
+}
+
 const courseHeaders = [
   { text: 'ID', dataKey: 'id' },
   { text: '과정명', dataKey: 'name' },
-  { text: '운영 기수', dataKey: 'activeGenerationCount' },
-  { text: '수강 인원', dataKey: 'totalEnrollmentCount' },
+  { text: '운영 기수', dataKey: 'active_generations_count' },
+  { text: '수강 인원', dataKey: 'total_students_count' },
   { text: '등록 일시', dataKey: 'created_at' },
   { text: '수정 일시', dataKey: 'updated_at' },
 ]
 
-const courseData = [
-  {
-    id: 1,
-    name: 'AI 백엔드 심화과정',
-    tag: 'AI1',
-    description: 'AI 백엔드 개발 심화 교육과정입니다.',
-    thumbnail_img_url: 'https://cdn.example.com/images/ai-course.png',
-    created_at: '2025-06-20T10:00:00Z',
-    updated_at: '2025-06-22T11:00:00Z',
-  },
-  {
-    id: 2,
-    name: '프론트엔드 기본 과정',
-    tag: 'FE1',
-    description: 'HTML, CSS, JS 기본기를 학습하는 과정',
-    thumbnail_img_url: 'https://cdn.example.com/images/fe-course.png',
-    created_at: '2025-06-21T09:00:00Z',
-    updated_at: null,
-  },
-]
-
 const Courses = () => {
+  const [fetchData, setFetchData] = useState<TableRowData[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<Error | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const courses = await fetchAPI()
+        setFetchData(courses)
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err)
+        } else {
+          console.error('알 수 없는 에러:', err)
+        }
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
   const { currentPage, totalPages, paginatedData, goToPage } = usePagination({
-    item: courseData,
+    item: fetchData,
     count: COUNT_LIMIT,
   })
+
+  if (loading)
+    return <div className="h-full text-center text-3xl">Loading...</div>
+  if (error) return <div>에러가 발생했습니다: {error.message}</div>
 
   return (
     <div className="w-[1600px] p-[30px]">
